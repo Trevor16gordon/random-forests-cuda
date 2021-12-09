f248e4d4b5e3c76d875d52580aa52ddcdac76fde
Trevor Gordon <trevor16gordon@gmail.com>
2021-12-09 10:44:29 -0500

    multiple decision trees appear to be working

--- src/python/random_forest.py
+++ src/python/random_forest.py
@@ -2,11 +2,14 @@ import pandas as pd
 import numpy as np
 from numpy import unravel_index
 from sklearn.model_selection import train_test_split
+from scipy.stats import mode
 from random import randrange
 from sklearn.metrics import confusion_matrix
+import pdb
 
 def checkTerminalCase(labels):
     return len(np.unique(labels)) == 1
+
 
 class RandomForestFromScratch():
 
@@ -19,96 +22,111 @@ class RandomForestFromScratch():
         self.d = len(X_train[0])
         self.weights = (1/self.X_n)*np.ones(self.X_n)
 
-        for iter in range(self.n_estimators):
+        self.dec_trees = []
+
+        for _ in range(self.n_estimators):
 
             choices = np.random.choice(range(self.X_n), p=self.weights, size=self.X_n)
             X_train_b = X_train[choices]
             y_train_b = y_train[choices]
 
-            item_stack_fifo = []
 
-            item_stack_fifo.append([X_train_b, y_train_b, 0, "start", None])
+            dt = DecisionTree(max_depth=self.max_depth)
+            dt.fit(X_train_b, y_train_b)
+            self.dec_trees.append(dt.root)
 
-            max_iters = 100
-            tot_count = 0
+    def predict(self, X):
 
-            root = None
+        all_y_pred = []
 
-            while item_stack_fifo and (tot_count < max_iters):
+        for tr in self.dec_trees:
+            all_y_pred.append(tr.predict(X))
 
-                tot_count += 1
-                [X_train_b, y_train_b, depth, node_dir,
-                    parent] = item_stack_fifo.pop(0)
-                n, _ = X_train_b.shape
+        all_res = np.array(all_y_pred)
+        most_popular = mode(all_res, axis=0).mode
+        return most_popular
 
-                is_terminal = checkTerminalCase(y_train_b) or ((depth + 1) >= self.max_depth)
-                if not is_terminal:
+class DecisionTree():
 
-                    if n == 0:
-                        # print("Not adding a node here")
-                        continue
+    def __init__(self, max_depth=3):
+        self.max_depth = max_depth
 
-                    all_gina_scores = np.zeros((n, self.d))
-                    # print(f"ewdbwiuebfuwbef n={n} and d ={self.d}")
-                    for dim in range(self.d):
-                        for row in range(n):
-                            # print(f"X_train_b={X_train_b} and its shape is {X_train_b.shape} and dim is {dim} and row is {row} and y_train_b {y_train_b}")
-                            # print(f"row{row} dim{dim}")
-                            all_gina_scores[row, dim] = self._calculate_score(X_train_b, y_train_b, dim, row)
-                    #print(all_gina_scores)
+    def fit(self, X_train, y_train):
+        self.X_n = len(X_train)
+        self.d = len(X_train[0])
+        self.weights = (1/self.X_n)*np.ones(self.X_n)
 
-                    max_index = self._choose_best_score(all_gina_scores)
-                    # print(f"best_split_value {best_split_val}, max_score_info {max_score_info}, max_index {max_index}")
 
-                    # print(f"X_train_b.shape {X_train_b.shape}")
-                    decision_boundary = X_train_b[max_index[0], max_index[1]]
+        choices = np.random.choice(range(self.X_n), p=self.weights, size=self.X_n)
+        X_train_b = X_train[choices]
+        y_train_b = y_train[choices]
 
-                    # print(f"node_dir {node_dir} current_depth: {depth}, best_dim {max_index[1]} best_split_val {best_split_val} decision_boundary {decision_boundary}")
+        item_stack_fifo = []
 
-                    
+        item_stack_fifo.append([X_train_b, y_train_b, 0, "start", None])
 
-                    X_train_left_b = X_train_b[X_train_b[:,max_index[1]] <= decision_boundary, :]
-                    X_train_right_b = X_train_b[X_train_b[:,max_index[1]] > decision_boundary, :]
-                    y_train_left_b = y_train_b[X_train_b[:,max_index[1]] <= decision_boundary, :]
-                    y_train_right_b = y_train_b[X_train_b[:,max_index[1]] > decision_boundary, :]
+        max_iters = 100
+        tot_count = 0
 
-                    if (len(X_train_left_b) == 0) or (len(X_train_right_b) == 0):
-                        print("Data didn't split into two. Making terminal here")
-                        is_terminal = True
+        root = None
 
-                if not is_terminal:
-                    node = TreeNode(dim=max_index[1], val=decision_boundary, depth=depth)
+        while item_stack_fifo and (tot_count < max_iters):
 
-                else: # Terminal Case
-                    (unique, counts) = np.unique(y_train_b, return_counts=True)
-                    sorted_count_values = [x for _, x in sorted(zip(counts, unique), reverse=True)]
-                    sorted_counts = [_ for _, x in sorted(zip(counts, unique), reverse=True)]
-                    highest_count_label = sorted_count_values[0]
-                    # print(f"highest_count_label is {highest_count_label}")
-                    # print(sorted_count_values)
-                    # print(sorted_counts)
-                    # pdb.set_trace()
-                    node = TreeNode(dim=None, val=None, depth=depth, is_terminal=True, terminal_val=highest_count_label)
-                    
+            tot_count += 1
+            [X_train_b, y_train_b, depth, node_dir,
+                parent] = item_stack_fifo.pop(0)
+            n, _ = X_train_b.shape
 
-                if parent is not None:
-                    if node_dir == "left":
-                        parent.left = node
-                    else:
-                        parent.right = node
+            is_terminal = checkTerminalCase(y_train_b) or ((depth + 1) >= self.max_depth)
+            if not is_terminal:
+
+                if n == 0:
+                    # print("Not adding a node here")
+                    continue
+
+                all_gina_scores = np.zeros((n, self.d))
+                # print(f"ewdbwiuebfuwbef n={n} and d ={self.d}")
+                for dim in range(self.d):
+                    for row in range(n):
+                        all_gina_scores[row, dim] = self._calculate_score(X_train_b, y_train_b, dim, row)
+
+                max_index = self._choose_best_score(all_gina_scores)
+                decision_bound = X_train_b[max_index[0], max_index[1]]
+                X_train_left_b = X_train_b[X_train_b[:,max_index[1]] <= decision_bound, :]
+                X_train_right_b = X_train_b[X_train_b[:,max_index[1]] > decision_bound, :]
+                y_train_left_b = y_train_b[X_train_b[:,max_index[1]] <= decision_bound, :]
+                y_train_right_b = y_train_b[X_train_b[:,max_index[1]] > decision_bound, :]
+
+                if (len(X_train_left_b) == 0) or (len(X_train_right_b) == 0):
+                    print("Data didn't split into two. Making terminal here")
+                    is_terminal = True
+
+            if not is_terminal:
+                node = TreeNode(dim=max_index[1], val=decision_bound, depth=depth)
+
+            # Terminal Case
+            else: 
+                (unique, counts) = np.unique(y_train_b, return_counts=True)
+                sorted_count_values = [x for _, x in sorted(zip(counts, unique), reverse=True)]
+                highest_count_label = sorted_count_values[0]
+                node = TreeNode(dim=None, val=None, depth=depth, is_terminal=True, terminal_val=highest_count_label)
+                
+
+            if parent is not None:
+                if node_dir == "left":
+                    parent.left = node
                 else:
-                    root = node
+                    parent.right = node
+            else:
+                root = node
 
-                if not is_terminal:
-                    
-                    item_stack_fifo.append([X_train_left_b, y_train_left_b, depth+1, "left", node])
-                    item_stack_fifo.append([X_train_right_b, y_train_right_b, depth+1, "right", node])
-                else:
-                    pass
-                    # print(f"Terminating {node_dir} node")
-
-            root.depth_first_print()
-            self.root = root
+            if not is_terminal:
+                
+                item_stack_fifo.append([X_train_left_b, y_train_left_b, depth+1, "left", node])
+                item_stack_fifo.append([X_train_right_b, y_train_right_b, depth+1, "right", node])
+            else:
+                pass
+        self.root = root
 
     def _calculate_score(self, X_train_b, y_train_b, dim, row):
 
@@ -132,25 +150,14 @@ class RandomForestFromScratch():
                 group2_counts[ii] = np.count_nonzero(group2 == cls_i) / len(group2)
 
         impurity = p1 * sum([x**2 for x in group1_counts]) + p2 * sum([x**2 for x in group2_counts])
-
-        # info =  {"sign": 1, "num_correct": impurity, "possible_correct": len(X_train_b), "split_value": split_value, "dim": dim}
-        
-
         return impurity
 
     def _choose_best_score(self, all_gina_scores):
-        # print("Checking the best score")
         max_list=[]
         max_list=np.flatnonzero(all_gina_scores == np.max(all_gina_scores))
-        print(max_list)
-        # print(f" number of best splits is {len(max_list)}")
-        # print(all_gina_scores)
-        
         max_list=unravel_index(max_list, all_gina_scores.shape)
         max_index_id=randrange(len(max_list[0]))    
-        max_index=[max_list[0][max_index_id], max_list[1][max_index_id]] 
-        # print(max_list)
-        # print(max_index)
+        max_index=[max_list[0][max_index_id], max_list[1][max_index_id]]
         return (max_index)
 
 
@@ -167,11 +174,7 @@ class TreeNode:
         self.label_dtype = object
 
     def depth_first_print(self, output_txt=""):
-        # print(
-        #     f"Node depth {self.depth} self.dim {self.dim} self.val {self.val}")
-
         if self.is_terminal:
-            # print(f"Terminal class is {self.terminal_val}")
             output_txt += "|   "*self.depth +  "|---" + f" class: {self.terminal_val}\n"
         else:
             output_txt += "|   "*self.depth +  "|---" + f"feature_{self.dim} >={self.val}\n"
@@ -202,15 +205,15 @@ class TreeNode:
         return labels
 
 
-
-df =pd.read_csv('data/IRIS.csv')
-X_all = df.iloc[:, 0:4]
-y_all = df.iloc[:, 4:]
-X_train, X_test, y_train, y_test = train_test_split(X_all, y_all, test_size=0.1)
-rf = RandomForestFromScratch(max_depth=3)
-rf.fit(X_train.to_numpy(), y_train.to_numpy())
-print("fit completed")
-rf.root.depth_first_print()
-predicts=rf.root.predict(X_test.to_numpy())
-confusion=confusion_matrix(predicts,y_test.to_numpy(),labels=['Iris-setosa','Iris-versicolor','Iris-virginica'])
-print(confusion)
+if __name__ == "__main__":
+    df =pd.read_csv('data/IRIS.csv')
+    X_all = df.iloc[:, 0:4]
+    y_all = df.iloc[:, 4:]
+    X_train, X_test, y_train, y_test = train_test_split(X_all, y_all, test_size=0.1)
+    rf = RandomForestFromScratch(max_depth=3)
+    rf.fit(X_train.to_numpy(), y_train.to_numpy())
+    print("fit completed")
+    rf.root.depth_first_print()
+    predicts=rf.root.predict(X_test.to_numpy())
+    confusion=confusion_matrix(predicts,y_test.to_numpy(),labels=['Iris-setosa','Iris-versicolor','Iris-virginica'])
+    print(confusion)
