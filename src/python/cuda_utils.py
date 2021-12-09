b83773de01a62dd09b8a232db7879909260e57c8
Trevor Gordon <trevor16gordon@gmail.com>
2021-12-09 13:21:45 -0500

    Moving Rakshith cuda functions to cuda_utils

--- src/python/cuda_utils.py
+++ src/python/cuda_utils.py
@@ -44,11 +44,81 @@ class DecisionTreeCudaUtils():
     
     def calculate_score(self, X_train_b, y_train_b, dim, row):
         # Implement CUDA function
-        pass
+        #Grid and block dimensions
+        blockDim=(1024,1024,1)
+        gridDim =(self.d//1024+1,self.d//1024+1,1)
+
+        #Converting to 32 bit
+        X_train_b = X_train_b.astype(np.float32)
+        y_train_b =y_train_b.astype(np.float32)
+        unique_classes=unique_classes.astype(np.float32)
+        row = X_train_b.shape[0].astype(np.float32)
+        dim = X_train_b.shape[1].astype(np.float32)
+
+        #Memory allocation
+        X_train_b_gpu = gpuArray.to_gpu(X_train_b)
+        y_train_b_gpu = gpuArray.to_gpu(y_train_b)
+        unique_classes_gpu=gpuArray.to_gpu(unique_classes)
+        impurity_scores_gpu = gpuArray.zeros_like(X_train_b_gpu)
+
+
+
+        #run and time the kernel
+        start.record()
+        calculate_gina_scores(impurity_scores_gpu,X_train_b_gpu,y_train_b_gpu,unique_classes_gpu,row,dim,block=blockDim,grid=gridDim)
+
+        # Wait for the event to complete
+        end.record()
+        end.synchronize()
+        time = start.time_till(end)
+
+        #Fetch the impurity scores
+        impurity_scores = impurity_scores_gpu.get()
+
+        #################Check with trevor about gina_info requirement
+        return impurity_scores,time
     
     def choose_best_score(self, scores: np.array):
-        # Implement CUDA function
-        pass
+        #Fetch the kernel
+        start =cuda.Event()
+        end = cuda.Event()
+
+        find_best_gina_score=self.mod.get("find_best_gina_score")
+
+        #Grid and block dimensions
+        blockDim=(1024,1024,1)
+        gridDim =(self.d//1024+1,self.d//1024+1,1)
+
+        #Converting to 32 bit
+        row,dim =np.float32(all_gina_scores.shape)
+        all_gina_scores=all_gina_scores.astype(np.float32)
+        
+
+        #memory allocation
+        all_gina_scores_gpu=gpuArray.to_gpu(all_gina_scores)
+
+        max_value =np.zeros(1)
+        max_value =max_value.astype(np.float32)
+        max_value_gpu=gpuArray.to_gpu(max_value)
+
+        index=np.zeros(2)
+        index=index.astype(np.float32)
+        index_gpu=gpuArray.to_gpu(index)
+
+
+        #run and time the kernel
+        start.record()
+        find_best_gina_score(max_value_gpu,index_gpu,all_gina_scores_gpu,row,dim,block=blockDim,grid=gridDim)
+
+        # Wait for the event to complete
+        end.record()
+        end.synchronize()
+        time = start.time_till(end)
+
+        #Fetch the impurity scores
+        index=index_gpu.get()
+
+        return(index,time)
 
     def split_data(self, X: np.array, y: np.array, bound: float, dim: float):
         # Implement CUDA function
