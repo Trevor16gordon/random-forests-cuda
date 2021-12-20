dd28f00a2d563d002918c646ffa099d8dec861a3
trevor16gordon <trevor16gordon@gmail.com>
2021-12-20 15:17:13 -0700

    commenting and cleaning up cuda utils

--- src/python/cuda_utils.py
+++ src/python/cuda_utils.py
@@ -34,8 +34,21 @@ class DecisionTreeCudaUtils():
         return SourceModule(kernelwrapper)
          
     def calculate_score(self, X_train_b, y_train_b):
-        
-        
+        """Calculate gini impurity for all X datapoints
+
+        Args:
+            X_train_b (np.array): The X training data with any number of rows and columns
+            y_train_b (np.array): The y categorical labeled data
+
+        Raises:
+            Exception: If X is not the correct data type
+            Exception: If y is not the correct data type
+
+        Returns:
+            tuple(np.array, list(TimingObjects))
+                np.array is the gini impurity scores with the same shape as X_train_b
+                list(TimingObjects)) contain useful timing information
+        """
         if not isinstance(X_train_b, np.ndarray):
             raise Exception("X_train_b needs to be np.array")
         if not isinstance(y_train_b, np.ndarray):
@@ -43,11 +56,8 @@ class DecisionTreeCudaUtils():
             
         y_train_b = y_train_b.astype(np.float32)
         X_train_b = X_train_b.astype(np.float32)
-            
-        # Implement CUDA function
-        
-
         unique_classes = np.unique(y_train_b)
+
         #Making categorical data into integers
         for j,label in enumerate(unique_classes):
             y_train_b[y_train_b == label]=j
@@ -56,7 +66,6 @@ class DecisionTreeCudaUtils():
         start1 =cuda.Event()
         start2 =cuda.Event()
         end = cuda.Event()
-
         calculate_scores=self.mod.get_function("calculate_gina_scores")
 
         #Converting to 32 bit
@@ -69,7 +78,6 @@ class DecisionTreeCudaUtils():
 
         #Grid and block dimensions
         blocksize=32
-#         blockDim=(blocksize,X_train_b.shape[1],1)
         blockDim=(blocksize, blocksize, 1)
         gridDim =(X_train_b.shape[0]//blocksize+1, X_train_b.shape[1]//blocksize+1, 1)
 
@@ -116,8 +124,27 @@ class DecisionTreeCudaUtils():
             num_cols=d)
         return impurity_scores, [time_obj, time_obj_non_mem_trans]
 
-
     def split_data(self, X: np.array, y: np.array, bound: float, dim: float):
+        """Split X and y arrays into two groups based on dimension and the boundary
+
+        Args:
+            X (np.array): 
+            y (np.array): 
+            bound (float): The condition to split the data using
+            dim (int): 
+
+        Raises:
+            Exception: If X is not the correct data type
+            Exception: If y is not the correct data type
+
+        Returns:
+            tuple:
+                X_l: np.array the left X array
+                y_l: np.array the left y array
+                X_r: np.array the right X array
+                y_r: np.array the right y array
+                Tuple of timing objects
+        """
         n, d = X.shape
         if not isinstance(X, np.ndarray):
             raise Exception("X needs to be np.array")
@@ -141,10 +168,7 @@ class DecisionTreeCudaUtils():
         col = np.array([X.shape[1]], dtype=np.int32)
         bound = np.array([bound], dtype=np.int32)
         dim =np.array([dim], dtype=np.int32)
-        #Memory allocation
-
         start1.record()
-
         X_gpu = gpuarray.to_gpu(X_32) 
         labels_gpu = gpuarray.to_gpu(labels)
 
@@ -184,102 +208,109 @@ class DecisionTreeCudaUtils():
             num_cols=d)
 
         return X_l, y_l, X_r, y_r, [time_obj, time_obj_non_mem_trans]
-
     
-    def choose_best_score2(self,all_gina_scores: np.array):
-        max_val_gpu = pycuda.gpuarray.max(a_cud)
-        max_val = max_val_gpu.get()
-        return np.where(all_gina_scores == max_val)
-
     def choose_best_score(self,all_gina_scores: np.array):
-      if not isinstance(all_gina_scores, np.ndarray):
+        """Find the maximum value in a 2d array.
+
+        Args:
+            all_gina_scores (np.array): The array of scores
+
+        Raises:
+            Exception: If the passed array is of the wrong type
+
+        Returns:
+            tuple(): max index and a list of timing objects
+        """
+        if not isinstance(all_gina_scores, np.ndarray):
             raise Exception("all_gina_scores needs to be np.array")
-      n, d = all_gina_scores.shape
-      #intialize cuda events
-      start1 =cuda.Event()
-      start2 =cuda.Event()
-      end = cuda.Event()
-      #fetching the kernel
-      scan = self.mod.get_function("reduction_scan")
+        
+        n, d = all_gina_scores.shape
 
-      #converting the 2d array into 1d for reduction
-      all_gina_scores = all_gina_scores.astype(np.float32)
-      all_gina_scores_flatten = all_gina_scores.flatten()
+        #intialize cuda events
+        start1 =cuda.Event()
+        start2 =cuda.Event()
+        end = cuda.Event()
+        #fetching the kernel
+        scan = self.mod.get_function("reduction_scan")
 
-      current_indexes = np.array([i for i in range(all_gina_scores_flatten.shape[0])])
-      current_indexes = current_indexes.astype(np.int32)
+        #converting the 2d array into 1d for reduction
+        all_gina_scores = all_gina_scores.astype(np.float32)
+        all_gina_scores_flatten = all_gina_scores.flatten()
 
-      # Get grid and block dim
-      BLOCKSIZE=1024
-      blockDim  = (BLOCKSIZE, 1, 1)
-      gridDim   = (len(current_indexes)// BLOCKSIZE+1, 1, 1)
+        current_indexes = np.array([i for i in range(all_gina_scores_flatten.shape[0])])
+        current_indexes = current_indexes.astype(np.int32)
 
-      #Making necessary sizes of all the auxillary blocks and sums
-      aux_length=len(current_indexes)//BLOCKSIZE+1
-      auxSum=np.zeros(aux_length)
-      auxSum=np.float32(auxSum)
-      auxSum2=np.zeros(32)
-      auxSum2=np.float32(auxSum2)
-      auxSum3=np.zeros(1)
-      auxSum3=np.float32(auxSum3)
-      
-      aux_length_2 = aux_length//BLOCKSIZE+1
-      auxIndex=np.zeros(aux_length, dtype=np.int32)
-      auxIndex2=np.zeros(aux_length_2, dtype=np.int32)
-      auxIndex3=np.zeros(1, dtype=np.int32)
-      aux_length=np.int32(aux_length)
+        # Get grid and block dim
+        BLOCKSIZE=1024
+        blockDim  = (BLOCKSIZE, 1, 1)
+        gridDim   = (len(current_indexes)// BLOCKSIZE+1, 1, 1)
 
-      start1.record()
-      
-      #Sending the info from h2d
-      input_gpu = gpuarray.to_gpu(all_gina_scores_flatten)
-      index_gpu = gpuarray.to_gpu(current_indexes)
-      auxSum_gpu = gpuarray.to_gpu(auxSum)
-      auxSum3_gpu = gpuarray.to_gpu(auxSum3)
-      auxSum2_gpu = gpuarray.to_gpu(auxSum2)
+        #Making necessary sizes of all the auxillary blocks and sums
+        aux_length=len(current_indexes)//BLOCKSIZE+1
+        auxSum=np.zeros(aux_length)
+        auxSum=np.float32(auxSum)
+        auxSum2=np.zeros(32)
+        auxSum2=np.float32(auxSum2)
+        auxSum3=np.zeros(1)
+        auxSum3=np.float32(auxSum3)
+        
+        aux_length_2 = aux_length//BLOCKSIZE+1
+        auxIndex=np.zeros(aux_length, dtype=np.int32)
+        auxIndex2=np.zeros(aux_length_2, dtype=np.int32)
+        auxIndex3=np.zeros(1, dtype=np.int32)
+        aux_length=np.int32(aux_length)
 
-      auxIndex_gpu = gpuarray.to_gpu(auxIndex)
-      auxIndex3_gpu = gpuarray.to_gpu(auxIndex3)
-      auxIndex2_gpu = gpuarray.to_gpu(auxIndex2)
-      
-      #kernel call
-      start2.record()
-      # (float* input, int* index,float* auxSum,int* auxIndex,int len)
-      scan(input_gpu,
-           index_gpu,
-           auxSum_gpu,
-           auxIndex_gpu,
-           np.int32(len(current_indexes)),
-           block=blockDim,grid=gridDim)
+        start1.record()
+        
+        #Sending the info from h2d
+        input_gpu = gpuarray.to_gpu(all_gina_scores_flatten)
+        index_gpu = gpuarray.to_gpu(current_indexes)
+        auxSum_gpu = gpuarray.to_gpu(auxSum)
+        auxSum3_gpu = gpuarray.to_gpu(auxSum3)
+        auxSum2_gpu = gpuarray.to_gpu(auxSum2)
 
-      #kernel call
-      scan(auxSum_gpu,
-           auxIndex_gpu,
-           auxSum2_gpu,
-           auxIndex2_gpu,
-           aux_length,
-           block=blockDim,grid=(32,1,1))
+        auxIndex_gpu = gpuarray.to_gpu(auxIndex)
+        auxIndex3_gpu = gpuarray.to_gpu(auxIndex3)
+        auxIndex2_gpu = gpuarray.to_gpu(auxIndex2)
+        
+        #kernel call
+        start2.record()
+        # (float* input, int* index,float* auxSum,int* auxIndex,int len)
+        scan(input_gpu,
+            index_gpu,
+            auxSum_gpu,
+            auxIndex_gpu,
+            np.int32(len(current_indexes)),
+            block=blockDim,grid=gridDim)
 
-      #kernel call
-      scan(auxSum2_gpu,
-           auxIndex2_gpu,
-           auxSum3_gpu,
-           auxIndex3_gpu,
-           np.int32(aux_length//BLOCKSIZE+1),
-           block=blockDim,grid=(1,1,1))
+        #kernel call
+        scan(auxSum_gpu,
+            auxIndex_gpu,
+            auxSum2_gpu,
+            auxIndex2_gpu,
+            aux_length,
+            block=blockDim,grid=(32,1,1))
 
-      # Wait for the event to complete
-      end.record()
-      end.synchronize()
-      elapsed_with_mem = start1.time_till(end)*1e-3
-      elapsed_without_mem = start2.time_till(end)*1e-3
+        #kernel call
+        scan(auxSum2_gpu,
+            auxIndex2_gpu,
+            auxSum3_gpu,
+            auxIndex3_gpu,
+            np.int32(aux_length//BLOCKSIZE+1),
+            block=blockDim,grid=(1,1,1))
 
-      # Fetch result from device to host
-      auxIndex3=auxIndex3_gpu.get()
+        # Wait for the event to complete
+        end.record()
+        end.synchronize()
+        elapsed_with_mem = start1.time_till(end)*1e-3
+        elapsed_without_mem = start2.time_till(end)*1e-3
 
-      max_index=unravel_index(int(auxIndex3), all_gina_scores.shape)
+        # Fetch result from device to host
+        auxIndex3=auxIndex3_gpu.get()
 
-      time_obj = TimingObject(
+        max_index=unravel_index(int(auxIndex3), all_gina_scores.shape)
+
+        time_obj = TimingObject(
             time=elapsed_with_mem,
             mem_transfer_included=True, 
             gpu_or_naive="gpu",
@@ -287,7 +318,7 @@ class DecisionTreeCudaUtils():
             num_rows=n,
             num_cols=d)
 
-      time_obj_non_mem_trans = TimingObject(
+        time_obj_non_mem_trans = TimingObject(
             time=elapsed_without_mem,
             mem_transfer_included=False, 
             gpu_or_naive="gpu",
@@ -295,4 +326,4 @@ class DecisionTreeCudaUtils():
             num_rows=n,
             num_cols=d)
 
-      return max_index, [time_obj, time_obj_non_mem_trans]
+        return max_index, [time_obj, time_obj_non_mem_trans]
